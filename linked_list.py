5c8e5e47dc7e17ddf614a79637479e14a0248800
Drfalcoew <drfalcoew@gmail.com>
2022-03-12 19:01:04 -0800

    Fixed linked list

--- linked_list.py
+++ linked_list.py
@@ -5,6 +5,7 @@ class Node:
     def __init__(self, value, next_node=None):
         self.value = value
         self.next_node = next_node
+
     def get_value(self):
         return self.value
     def get_next_node(self):
@@ -16,7 +17,7 @@ class Node:
 
 class LinkedList:
     def __init__(self, value=None):
-        self.head_node = Node(value)
+        self.head_node = None if value == None else Node(value)
 
     def __repr__(self):  # Returns a string representation of the list
         stringified_list = ""
@@ -48,74 +49,85 @@ class LinkedList:
                     current_node = current_node.get_next_node()
 
 
+    def length(self):
+        total = 0
+        temp_node = self.head_node
+        while temp_node:
+            temp_node = temp_node.get_next_node()
+            total+=1
+        return total
 
-    def swap_nodes(self, value_a,  value_b):
-        print("Swapping Nodes")
-        node_a = self.head_node
-        node_b = self.head_node
-        node_a_prev = None
-        node_b_prev = None
-        if value_a == value_b:
-            print("Swap not necessary. Values are equal")
+    def get_at_index(self, index):
+        if self.length() <= index < 0:
+            return None
+
+
+        temp_node = self.head_node
+        temp_index = 0
+        while temp_node:
+            print('passed index: {}, temp index: {}'.format(index, temp_index))
+            if index == temp_index:
+                return temp_node.get_value()
+            temp_node = temp_node.get_next_node()
+            temp_index+=1
+
+
+
+
+    def swap_nodes(self, x,  y):
+        if x == y:
             return
 
-        while node_a:
-            print("In while")
-            #  FINDING THE NODES BY THEIR VALUES
+            # Search for x (keep track of prevX and CurrX)
+        prevX = None
+        currX = self.head_node
+        while currX != None and currX.value != x:
+            prevX = currX
+            currX = currX.next_node
 
-            if node_a.get_value() == value_a:
-                print(f"Found value a: {node_a.get_value()}")
-                break  # If value_a is found, break
-            node_a_prev = node_a  # Else, traverse forward, by setting previous node = to current node, and current node = to next node.
-            node_a = node_a.get_next_node()
+        # Search for y (keep track of prevY and currY)
+        prevY = None
+        currY = self.head_node
+        while currY != None and currY.value != y:
+            prevY = currY
+            currY = currY.next_node
 
-        while node_b:
-
-            if node_b.get_value() == value_b:
-                print(f"Found value b: {node_b.get_value()}")
-                break  # Same system for finding value_b
-            node_b_prev = node_b
-            node_b = node_b.get_next_node()
-
-
-
-        if node_a is None or node_b is None:  # Swap not possible; One or more values are not found within the list.
-            print("Swap not possible")
+        # If either x or y is not present, nothing to do
+        if currX == None or currY == None:
             return
+        # If x is not head of linked list
+        if prevX != None:
+            prevX.next_node = currY
+        else:  # make y the new head
+            self.head_node = currY
 
-        # LINKING THE BACK OF THE NODES TO THEIR PREDECESSORS
+        # If y is not head of linked list
+        if prevY != None:
+            prevY.next_node = currX
+        else:  # make x the new head
+            self.head_node = currX
 
-        if node_a_prev is None:  # If none is before node_a, then node_a must be the head (beginning)
-            self.head_node = node_b  # Swapping b for a
-        else:
-            node_a_prev.set_next_node(node_b)
-
-        if node_b_prev is None:
-            self.head_node = node_a
-        else:
-            node_b_prev.set_next_node(node_a)
-
-        #  LINKING THE FRONT OF THE NODES TO THEIR SUCCESSORS
-
-        temp = node_a.get_next_node
-        node_a.set_next_node(node_b.get_next_node())
-        node_b.set_next_node(temp)
+        # Swap next pointers
+        temp = currX.next_node
+        currX.next_node = currY.next_node
+        currY.next_node = temp
 
 
 def main():
-    ll = LinkedList(1)
-
-    for i in range(2, 16):
+    ll = LinkedList()
+    for i in range(10, 0, -1):
+        print(i)
         ll.push_node(i)
 
     print(ll)
 
-    ll.remove_node(10)
-    print(ll)
+    ll.remove_node(8)
     print()
     print()
-    ll.swap_nodes(15, 9)
+    ll.swap_nodes(4, 5)
     print(ll)
+    x = input("Get value at index: ")
+    print("\n\nMy value at index {} = {}".format(x, ll.get_at_index(int(x))))
 
 
 
